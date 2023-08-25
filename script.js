3e93f3e8261ba330f82dc7ef80c9331a8ff49238
Dushyant-Mahto <dmahto2001@gmail.com>
2023-08-25 23:40:02 +0530

    Naavbar and Product updates

--- script.js
+++ script.js
@@ -3,4 +3,19 @@ $(document).ready(function(){
         $("i",this).toggleClass('fa fa-times');
         $('.navbar').toggleClass('nav-toggle');
     });
+    $(window).on('scroll load', function(){
+        $('#menu').removeClass('fa fa-times');
+        $('.navbar').removeClass('nav-toggle');
+        $('section').each(function(){
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let id = $(this).attr('id');
+
+            if(top>= offset && top< offset+height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find('[href=`#${id}`]').addClass('active');
+            }
+        });
+    });
 });
